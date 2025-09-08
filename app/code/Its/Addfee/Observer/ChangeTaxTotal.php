041fbde3dbc85b6e1f7b40965347e3349493ec17
Dmitrii Fediuk <dfediuk@gmail.com>
2025-09-08 12:21:54 +0100

    https://github.com/keyclampstore-com/m/issues/4

--- app/code/Its/Addfee/Observer/ChangeTaxTotal.php
+++ app/code/Its/Addfee/Observer/ChangeTaxTotal.php
@@ -1,13 +1,8 @@
 <?php
 namespace Its\Addfee\Observer;
-
 use \Magento\Framework\Event\ObserverInterface;
 use \Magento\Framework\Event\Observer;
-
-class ChangeTaxTotal implements ObserverInterface
-{
-    public $additionalTaxAmt = 20;
-
+class ChangeTaxTotal implements ObserverInterface {
     function execute(Observer $observer) {
         /** @var Magento\Quote\Model\Quote\Address\Total */
         $total = $observer->getData('total');
