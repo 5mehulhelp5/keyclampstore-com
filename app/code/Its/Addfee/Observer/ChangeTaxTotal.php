2bcb50e9efe858f793e8547dfd27bd38aed8c05f
Dmitrii Fediuk <dfediuk@gmail.com>
2025-09-08 12:28:12 +0100

    https://github.com/keyclampstore-com/m/issues/4

--- app/code/Its/Addfee/Observer/ChangeTaxTotal.php
+++ app/code/Its/Addfee/Observer/ChangeTaxTotal.php
@@ -5,6 +5,10 @@ use Magento\Framework\Event\ObserverInterface;
 use Magento\Quote\Model\Quote as Q;
 use Magento\Quote\Model\Quote\Address\Total as T;
 final class ChangeTaxTotal implements ObserverInterface {
+	/**
+	 * 2025-09-08 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * "Refactor the `Its_Addfee` module": https://github.com/keyclampstore-com/m/issues/4
+	 */
     function execute(O $o):void {
         $t = $o['total']; /** @var T $t */
 		$q = $o['quote']; /** @var Q $q */
